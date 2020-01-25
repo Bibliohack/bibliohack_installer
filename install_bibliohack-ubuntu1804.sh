627d5759b6061d52449869bb91d48c5fb8f0069c
d-a-l <minombresbond@gmail.com>
2020-01-25 08:02:43 -0300

    offline/online installation and bugfix

--- install_bibliohack-ubuntu1804.sh
+++ install_bibliohack-ubuntu1804.sh
@@ -37,8 +37,10 @@ PDFBEADS_DIR="$COMPONENTS/pdfbeads-kopi"
 CHDKPTP_DIR="$RESOURCES/chdkptp-461"
 TECGRAF_DIR="$RESOURCES/tecgraf"
 
+OFFLINE_INSTALLATION="NO"
+
 if [[ "$2" == "reset" ]]; then
-        [[ -d "$RESOURCES/tecgraf" ]] && {
+   [[ -d "$RESOURCES/tecgraf" ]] && {
 		echo "desinstalando Tecgraf..."
 		$INSTALADORES_DIR/tecgraf uninstall tecgraf_dir="$RESOURCES/tecgraf" || exit 1
 	}
@@ -64,9 +66,11 @@ if [[ "$2" == "reset" ]]; then
 	echo "Instalación previa eliminada."
 	echo "use:"
 	echo "> cd$INSTALADORES_DIR" 
-        echo "> $0 $1"
-        echo "para reiniciar la instalación."
+   echo "> $0 $1"
+   echo "para reiniciar la instalación."
 	exit 0
+elif [[ "$2" == "offline" ]]; then
+	OFFLINE_INSTALLATION="YES" 
 fi
 
 [[ -d "$BIBLIOHACK_DIR" ]] || {
@@ -301,7 +305,15 @@ echo "instalando 'build essential'"
 # BUILD_ESSENTIAL="INSTALLED"
 
 if [ "$BUILD_ESSENTIAL" != "INSTALLED" ]; then
-	dpkg_install "$BIBLIOHACK_ORIG/deb/build-essential"
+	if [ "$OFFLINE_INSTALLATION" == "YES" ]
+	 then
+ 	   dpkg_install "$BIBLIOHACK_ORIG/deb/build-essential" || { error_msg "fallo dpkg build-essential"; exit 1; }
+ 	else
+ 	   sudo apt-get -y install build-essential cmake checkinstall git || {
+	  		>&2 echo "ERROR al instalar build-essential o sus dependencias"
+	  		exit 1
+	  	}
+ 	fi
 fi
 
 # --------------------------------------------------------
@@ -322,8 +334,9 @@ if ! tecgraf_check; then
 	[[ -d "$TECGRAF_DIR" ]] || {
 		mkdir "$TECGRAF_DIR" || exit 1
 	}
-   echo $INSTALADORES_DIR/tecgraf install profile=d tecgraf_dir="$TECGRAF_DIR" orig_dpkg_dir="$TECGRAF_ORIG_DPKG_DIR" orig_dir="$TECGRAF_ORIG"
-	$INSTALADORES_DIR/tecgraf install profile=d tecgraf_dir="$TECGRAF_DIR" orig_dpkg_dir="$TECGRAF_ORIG_DPKG_DIR" orig_dir="$TECGRAF_ORIG" || exit 1
+
+   echo $INSTALADORES_DIR/tecgraf install profile=d tecgraf_dir="$TECGRAF_DIR" orig_dpkg_dir="$TECGRAF_ORIG_DPKG_DIR" orig_dir="$TECGRAF_ORIG" offline_installation="$OFFLINE_INSTALLATION"
+	$INSTALADORES_DIR/tecgraf install profile=d tecgraf_dir="$TECGRAF_DIR" orig_dpkg_dir="$TECGRAF_ORIG_DPKG_DIR" orig_dir="$TECGRAF_ORIG" offline_installation="$OFFLINE_INSTALLATION" || exit 1
    echo "$tecgraf_ok"
 fi
 
@@ -359,8 +372,8 @@ if ! chdkptp_check; then
 	[[ -d "$CHDKPTP_DIR" ]] || {
 		mkdir "$CHDKPTP_DIR" || exit 1
 	}
-	echo $INSTALADORES_DIR/chdkptp install profile=d tecgraf_dir="$TECGRAF_DIR" chdkptp_dir="$CHDKPTP_DIR" orig_dpkg_dir="$CHDKPTP_ORIG_DPKG_DIR" orig_path="$CHDKPTP_SRC_TAR_PATH"
-	$INSTALADORES_DIR/chdkptp install profile=d tecgraf_dir="$TECGRAF_DIR" chdkptp_dir="$CHDKPTP_DIR" orig_dpkg_dir="$CHDKPTP_ORIG_DPKG_DIR" orig_path="$CHDKPTP_SRC_TAR_PATH" || error_msg "fallo la instalacion de chdkptp"
+	echo $INSTALADORES_DIR/chdkptp install profile=d tecgraf_dir="$TECGRAF_DIR" chdkptp_dir="$CHDKPTP_DIR" orig_dpkg_dir="$CHDKPTP_ORIG_DPKG_DIR" orig_path="$CHDKPTP_SRC_TAR_PATH" offline_installation="$OFFLINE_INSTALLATION"
+	$INSTALADORES_DIR/chdkptp install profile=d tecgraf_dir="$TECGRAF_DIR" chdkptp_dir="$CHDKPTP_DIR" orig_dpkg_dir="$CHDKPTP_ORIG_DPKG_DIR" orig_path="$CHDKPTP_SRC_TAR_PATH" offline_installation="$OFFLINE_INSTALLATION" || error_msg "fallo la instalacion de chdkptp"
    echo -e "$chdkptp_ok \n\n"
 else
 	echo "chdkptp ya instalado"
@@ -383,7 +396,15 @@ echo "instalando pdfbeads"
 
 if ! pdfbeads_check; then
 
-	dpkg_install "$PDFBEADS_ORIG_DPKG_DIR"
+	if [ "$OFFLINE_INSTALLATION" == "YES" ]
+	 then
+		dpkg_install "$PDFBEADS_ORIG_DPKG_DIR" || { error_msg "fallo dpkg pdfbeads"; exit 1; }
+ 	else
+      sudo apt-get install -y ruby ruby-dev ri ruby-full ruby2.5 ruby2.5-dev rake ruby-rmagick ruby-pkg-config ruby-fast-xs ruby-nokogiri ruby-hpricot || {
+	  		>&2 echo "ERROR al instalar dependencias de pdfbeads-kopi"
+	  		exit 1
+	  	}
+ 	fi
 
    verify_iconv=$(gem list -i iconv)
 	if [ "$verify_iconv" == "false" ]; then
@@ -436,7 +457,17 @@ EOF
 )
 
 if ! tesseract_check; then
-	dpkg_install "$TESSERACT_ORIG_DPKG_DIR" || exit 1
+
+	if [ "$OFFLINE_INSTALLATION" == "YES" ]
+	 then
+		dpkg_install "$TESSERACT_ORIG_DPKG_DIR" || { error_msg "fallo dpkg tesseract"; exit 1; }
+ 	else
+      sudo apt-get -y install tesseract-ocr tesseract-ocr-ita tesseract-ocr-rus tesseract-ocr-por tesseract-ocr-deu tesseract-ocr-ell tesseract-ocr-fra tesseract-ocr-lat tesseract-ocr-eng tesseract-ocr-spa tesseract-ocr-spa-old tesseract-ocr-nld tesseract-ocr-osd tesseract-ocr-cat libopenjp2-7 || {
+	  		>&2 echo "ERROR al instalar tesseract-ocr o sus dependencias"
+	  		exit 1
+	  	}
+ 	fi
+
    echo -e "$tesseract_ok \n\n"
 fi
 
@@ -466,7 +497,20 @@ scantailor_instalado=""
 # fi
 
 if ! scantailor_universal_check; then
-	dpkg_install "$SCANTAILOR_UNIVERSAL_ORIG_DPKG_DIR" || error_msg "fallo dpkg scantailor universal"
+
+	if [ "$OFFLINE_INSTALLATION" == "YES" ]
+	 then
+		dpkg_install "$SCANTAILOR_UNIVERSAL_ORIG_DPKG_DIR" || { error_msg "fallo dpkg scantailor universal"; exit 1; }
+ 	else
+  		sudo apt-get -y install libmng2 libqt4-xml libqt4-dbus libqt4-xmlpatterns qtcore4-l10n qdbus libqtdbus4 libqt4-declarative libqt4-script \
+  										libqtgui4 libqt4-sql-mysql libqtcore4 libqt4-network libqt4-sql qtchooser mysql-common qt-at-spi libmysqlclient20 \
+										libdouble-conversion1 libxcb-xinerama0 libqt5xml5 qt5-gtk-platformtheme libqt5dbus5 libqt5gui5 libqt5network5 \
+										libqt5widgets5 libqt5core5a libqt5svg5 qttranslations5-l10n || {
+	  		>&2 echo "ERROR al instalar scantailor-universal o sus dependencias"
+	  		exit 1
+	  	}
+ 	fi
+
 	CUSTOM_TARDIR="${SCANTAILOR_UNIVERSAL_DIR##*/}"
 	echo cp_and_untar "$SCANTAILOR_UNIVERSAL_TAR_PATH" "$COMPONENTS" "$CUSTOM_TARDIR"
 	cp_and_untar "$SCANTAILOR_UNIVERSAL_TAR_PATH" "$COMPONENTS" "$CUSTOM_TARDIR" || error_msg "fallo cp_and_untar para scantailor universal"
@@ -477,7 +521,18 @@ else
 fi
 
 if ! scantailor_advanced_check; then
-	dpkg_install "$SCANTAILOR_ADVANCED_ORIG_DPKG_DIR" || error_msg "fallo dpkg scantailor advanced"
+
+	if [ "$OFFLINE_INSTALLATION" == "YES" ]
+	 then
+		dpkg_install "$SCANTAILOR_ADVANCED_ORIG_DPKG_DIR" || { error_msg "fallo dpkg scantailor advanced"; exit 1; }
+ 	else
+  		sudo apt-get -y install libdouble-conversion1 libxcb-xinerama0 libqt5opengl5 qt5-gtk-platformtheme libqt5dbus5 libqt5gui5 libqt5network5 \
+  										libqt5widgets5 libqt5core5a libqt5svg5 qttranslations5-l10n || {
+	  		>&2 echo "ERROR al instalar scantailor-advanced o sus dependencias"
+	  		exit 1
+	  	}
+ 	fi
+
 	CUSTOM_TARDIR="${SCANTAILOR_ADVANCED_DIR##*/}"
 	echo cp_and_untar "$SCANTAILOR_ADVANCED_TAR_PATH" "$COMPONENTS" "$CUSTOM_TARDIR"
 	cp_and_untar "$SCANTAILOR_ADVANCED_TAR_PATH" "$COMPONENTS" "$CUSTOM_TARDIR" || error_msg "fallo cp_and_untar para scantailor advanced"
@@ -500,7 +555,17 @@ dalclick_ok=$(cat <<EOF
 EOF
 )
 if ! dalclick_check; then
-   dpkg_install "$DALCLICK_ORIG_DPKG_DIR" || exit 1
+
+	if [ "$OFFLINE_INSTALLATION" == "YES" ]
+	 then
+   	dpkg_install "$DALCLICK_ORIG_DPKG_DIR" || { error_msg "fallo dpkg dalclick"; exit 1; }
+ 	else
+  		sudo apt-get -y install exactimage bc geeqie evince || {
+	  		>&2 echo "ERROR al instalar dalclick o sus dependencias"
+	  		exit 1
+	  	}
+ 	fi
+
    CUSTOM_TARDIR="${DALCLICK_DIR##*/}"
    cp_and_untar "$DALCLICK_SRC_TAR_PATH" "$BIBLIOHACK_DIR" "$CUSTOM_TARDIR" || exit 1
    BIB_DIR="/bib/$INSTITUCION_ID"
